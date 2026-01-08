d1c992e64bd12863ec2a0d2a8c9a2266fbc3777f
JayCorp97 <jayasankaj@gmail.com>
2026-01-09 10:03:05 +1100

    Implement user registration and authentication features, enhance dashboard layout, and add error handling

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: String,
-  email: { type: String, unique: true },
-  password: String
+const userSchema = new mongoose.Schema({
+  f_name: {
+    type: String,
+    required: true
+  },
+  l_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  active: {
+    type: Number,
+    default: 1
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
